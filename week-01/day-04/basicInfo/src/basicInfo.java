962698045ce347be0eeb22edea9f06f315a8440d
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-05 16:54:02 +0100

    basic info java

---
+++ week-01/day-04/basicInfo/src/basicInfo.java
@@ -0,0 +1,29 @@
+public class basicInfo {
+    public static void main(String[] args) {
+
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String myName;
+        myName = "Orsi";
+
+        System.out.println("name " + myName);
+
+        int myAge;
+        myAge = 45;
+
+        System.out.println("age " + myAge);
+
+        double myHeight;
+        myHeight = 1.73;
+
+        System.out.println("height " + myHeight + " m");
+
+        boolean married = false;
+
+        System.out.println("is married? " + married);
+    }
+}
