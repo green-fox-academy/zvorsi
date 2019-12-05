42f2b908f6fd035d421816b01cffecc82a67c300
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-05 16:53:33 +0100

    adding BMI java

---
+++ week-01/day-04/BMI/src/bmi.java
@@ -0,0 +1,13 @@
+public class bmi {
+    public static void main(String[] args) {
+
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        // Print the Body mass index (BMI) based on these values
+
+        System.out.println("weight: " + massInKg + " kg");
+        System.out.println("height: " + heightInM + " m");
+        System.out.println("BMI: " + massInKg / (heightInM * heightInM));
+    }
+}
