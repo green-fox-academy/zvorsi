7f9f7a806844c37a5427b8900103234fc41d562a
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-06 16:25:53 +0100

    first loop

---
+++ week-01/day-05/wontCheat/src/wontCheat.java
@@ -0,0 +1,12 @@
+public class wontCheat {
+    public static void main(String[] args) {
+
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
+
+        String A = "I wont cheat on the exam!";
+        for ( int i = 0; i < 100; i++ ){
+            System.out.println(( i + 1 ) + " " + A);
+        }
+    }
+}
