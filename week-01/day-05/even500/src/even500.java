f06027e867fc304a33e7a95c699e94f91e3578b3
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-06 16:52:15 +0100

    even 500

---
+++ week-01/day-05/even500/src/even500.java
@@ -0,0 +1,12 @@
+public class even500 {
+    public static void main(String[] args) {
+        // Create a program that prints all the even numbers between 0 and 500
+
+        int a = 0;
+        while (a < 500) {
+            System.out.println(a + 2);
+            a += 2;
+        }
+    }
+
+}
