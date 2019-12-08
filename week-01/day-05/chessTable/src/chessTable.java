33b4ff9e5dc0e3126cc171be8d3b5dd3c5f97bfc
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-08 19:54:44 +0100

    chesstable

---
+++ week-01/day-05/chessTable/src/chessTable.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class chessTable {
+    public static void main(String[] args) {
+
+        // Crate a program that draws a chess table like this
+        //
+        // % % % %
+        //  % % % %
+        // % % % %
+        //  % % % %
+        // % % % %
+        //  % % % %
+        // % % % %
+        //  % % % %
+        //
+
+        String light = "[]";
+        String dark = "#";
+        int table = 8;
+
+        for (int i = 0; i < table; i++) {
+            if (i % 2 == 0) {
+                System.out.println(dark + light + dark + light + dark + light + dark + light);
+            } else {
+                System.out.println(light + dark + light + dark + light + dark + light + dark);
+            }
+        }
+    }
+}
+
+
