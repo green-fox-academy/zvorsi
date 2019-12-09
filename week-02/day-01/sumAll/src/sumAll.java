2b66a1697eadef2d8fda63cb7fc45cf0322597ab
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-09 21:48:29 +0100

    simple solution

---
+++ week-02/day-01/sumAll/src/sumAll.java
@@ -0,0 +1,12 @@
+public class sumAll {
+    public static void main(String[] args) {
+
+        // - Create an array variable named `ai`
+        //   with the following content: `[3, 4, 5, 6, 7]`
+        // - Print the sum of the elements in `ai`
+        int[] ai = {3, 4, 5, 6, 7};
+        int sum = ai[0] + ai[1] + ai[2] + ai[3] + ai[4];
+
+        System.out.println(sum);
+    }
+}
