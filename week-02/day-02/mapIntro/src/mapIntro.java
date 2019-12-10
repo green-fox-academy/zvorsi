6fcdefc1c3783e3f9d6e2e72b8c845101280261b
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-10 17:04:12 +0100

    funny little maps thingy

---
+++ week-02/day-02/mapIntro/src/mapIntro.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class mapIntro {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        if (map.size() != 0) {
+            System.out.println(map.size());
+        }
+
+        map.put(97, "a");
+        map.put(98, "b");
+        map.put(99, "c");
+        map.put(65, "A");
+        map.put(66, "B");
+        map.put(67, "C");
+
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+
+        map.put(68, "D");
+
+        System.out.println(map.size());
+
+        System.out.println(map.get(99));
+
+        map.remove(99);
+
+        if (map.containsKey(100)){
+            System.out.println("map has a key: 100");
+        }else{
+            System.out.println("map hasn't got key: 100");
+        }
+
+        map.clear();
+
+    }
+}
+
