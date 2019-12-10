de9fff605316c8b5f70aa2417c2cc5bdf2a3639a
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-10 13:07:38 +0100

    string part replaced

---
+++ week-02/day-02/simpleReplace/src/simpleReplace.java
@@ -0,0 +1,10 @@
+public class simpleReplace {
+    public static void main(String[] args) {
+
+        String example = "In a dishwasher far far away";
+        String replaced = example.replaceFirst("dishwasher","galaxy");
+
+        System.out.println(replaced);
+        
+    }
+}
