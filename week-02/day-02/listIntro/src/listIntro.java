59d04797d21cb32b2fd8224ac318ef113f9f469c
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-10 16:21:16 +0100

    a clear lefarasztott

---
+++ week-02/day-02/listIntro/src/listIntro.java
@@ -0,0 +1,40 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
+import java.util.ArrayList;
+
+public class listIntro {
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        System.out.println(names.size());
+
+        names.add("William");
+
+        System.out.println(names.size());
+
+        names.add("John");
+        names.add("Amanda");
+
+        System.out.println(names.size());
+
+        System.out.println(names.get(2));
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println("elements: " + names.get(i));
+        }
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println((i + 1) + ". " + names.get(i));
+        }
+
+        names.remove(1);
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println("elements: " + names.get(i));
+        }
+        names.clear();
+
+        System.out.println(names.size());
+    }
+}
