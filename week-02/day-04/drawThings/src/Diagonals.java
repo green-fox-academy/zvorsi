810fd45db55844547d0b740de69dfba2f6b103b8
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-12 13:17:18 +0100

    red green diagonals

---
+++ week-02/day-04/drawThings/src/Diagonals.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Diagonals {
+    public static void mainDraw(Graphics graphics) {
+        
+        int x = 0;
+        int y = 0;
+
+        graphics.setColor(Color.RED);
+        graphics.drawLine(WIDTH, x, y, HEIGHT);
+
+        if (x == 0 || y == 0) {
+            graphics.setColor(Color.GREEN);
+        }
+
+        graphics.drawLine(x, y, HEIGHT, WIDTH);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+
+}
