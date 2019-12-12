e486f5143f073fc6a601e90794d1762347dc3922
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2019-12-12 11:20:29 +0100

    colored box, will try later to use a for cycle

---
+++ week-02/day-04/drawThings/src/coloredBox.java
@@ -0,0 +1,47 @@
+import javax.swing .*;
+import java.awt .*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class coloredBox {
+    public static void mainDraw(Graphics graphics) {
+        // Draw a box that has different colored lines on each edge.
+
+        graphics.setColor(Color.RED);
+        graphics.drawLine(25, 25, 125, 25);
+
+        graphics.setColor(Color.ORANGE);
+        graphics.drawLine(125, 25, 125, 125);
+
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(125, 125, 25, 125);
+
+        graphics.setColor(Color.BLUE);
+        graphics.drawLine(25, 125, 25, 25);
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
