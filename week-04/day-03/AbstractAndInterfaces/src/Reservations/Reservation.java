3bfc71c8b83fa0baad537dfdb43a68604ec0d889
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2020-01-09 10:43:37 +0100

    reservation works

--- week-04/day-03/AbstractAndInterfaces/src/Reservations/Reservation.java
+++ week-04/day-03/AbstractAndInterfaces/src/Reservations/Reservation.java
@@ -1,6 +1,8 @@
 package Reservations;
 
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Reservation implements Reservationy{
@@ -10,14 +12,19 @@ public class Reservation implements Reservationy{
     @Override
     public String getDowBooking() {
         int randomDay = 1 + random.nextInt(6);
-        String name = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}[randomDay];
-        return name;
+        String day = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}[randomDay];
+        return day;
     }
 
 
     @Override
     public String getCodeBooking() {
-
-        return null;
+        String result = "";
+        for (int i = 0; i < 8 ; i++) {
+            int randomChar = 1 + random.nextInt(35);
+            String code = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}[randomChar];
+            result+= code;
+        }
+        return result;
     }
 }
--- week-04/day-03/AbstractAndInterfaces/src/Reservations/main.java
+++ week-04/day-03/AbstractAndInterfaces/src/Reservations/main.java
@@ -4,7 +4,10 @@ public class main {
     public static void main(String[] args) {
 
         Reservation res1 = new Reservation();
+        Reservation res2 = new Reservation();
+
 
         System.out.println("Booking# " + res1.getCodeBooking() + " for day " + res1.getDowBooking());
+        System.out.println("Booking# " + res2.getCodeBooking() + " for day " + res2.getDowBooking());
     }
 }
