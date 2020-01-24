70b94333978dc719d37b6c5c06340316cb259a74
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2020-01-24 09:47:31 +0100

    basic webshop is started

---
+++ week-06/day-05/BasicWebshop/src/test/java/com/orsi/webshop/basicwebshop/BasicwebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orsi.webshop.basicwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicwebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
