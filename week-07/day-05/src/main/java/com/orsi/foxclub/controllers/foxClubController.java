08168e124e21473f294cf2647d9e134f2548b6d3
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2020-01-31 11:53:59 +0100

    landing and login

--- week-07/day-05/src/main/java/com/orsi/foxclub/controllers/foxClubController.java
+++ week-07/day-05/src/main/java/com/orsi/foxclub/controllers/foxClubController.java
@@ -1,4 +1,26 @@
 package com.orsi.foxclub.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class foxClubController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@RequestParam String foxName){
+        return "index";
+    }
+
 }
