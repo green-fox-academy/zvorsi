024f4af51cfd51cadbb6da41822a1d8a240171c5
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2020-03-19 15:17:48 +0100

    first js try

---
+++ js-basics/index.js
@@ -0,0 +1,33 @@
+'use strict';
+console.log('Hello world!');
+
+let name  = 'Orsi';
+console.log(`hello ${name}`);
+
+// humpty Dumpty
+console.log('Humpty Dumpty sat on a wall,');
+console.log('Humpty Dumpty had a great fall.');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put Humpty together again.');
+
+// Greet 3 of your classmates
+let greet = "Hello";
+console.log(greet + ", Esther!");
+console.log(greet + ", Mary!");
+console.log(greet + ", Joe!");
+
+// introduce yourself
+let age = 46;
+let height = 1.72;
+
+console.log(name);
+console.log(age);
+console.log(height);
+
+// coding hours
+let semester = 17;
+let daily = 6;
+let totalHours = semester * daily * 5;
+console.log(totalHours);
+let averageDailyWorkHours = 52 / 5;
+console.log((daily / averageDailyWorkHours * 100 ).toFixed(2)  + '%');
