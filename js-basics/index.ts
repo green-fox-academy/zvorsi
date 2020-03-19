df1757fc031a577f59f7e46a2b324be66f655364
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2020-03-19 18:06:05 +0100

    fizzbuzz correct

--- js-basics/index.ts
+++ js-basics/index.ts
@@ -76,11 +76,9 @@ console.log(c);
 for (let i: number = 1; i < 101; i++){
     if(i % 3 === 0){
         console.log("fizz");
-    }
-    if (i % 5 === 0){
+    } else if (i % 5 === 0){
         console.log("buzz");
-    }
-    if (i % 3 === 0 && i % 5 === 0){
+    } else if (i % 3 === 0 && i % 5 === 0){
         console.log("fizzbuzz");
     }else{
         console.log(i);
