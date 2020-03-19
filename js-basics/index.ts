d1f73295b7bb6f012d5b68c1e2fad48dcf8a8d4b
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2020-03-19 17:36:16 +0100

    fzzbuzz

--- js-basics/index.ts
+++ js-basics/index.ts
@@ -1,4 +1,5 @@
 'use strict';
+
 console.log('Hello world!');
 
 let myName: string = 'Orsi';
@@ -31,3 +32,57 @@ let totalHours: number = semester * daily * 5;
 console.log(totalHours);
 let averageDailyWorkHours: number = 52 / 5;
 console.log((daily / averageDailyWorkHours * 100 ).toFixed(2)  + '%');
+
+
+//cuboid
+let sideA: number = 2;
+let sideB: number = 3;
+let sideC: number = 4;
+console.log("surface area: " + 2 * (sideA * sideB + sideA * sideC + sideB * sideC));
+console.log("volume: " + sideA * sideB * sideC);
+
+
+//conditional variable mutation
+let a: number = 24;
+let out: number = 0;
+if(a % 2 == 0){
+    out++;
+}
+console.log(out);
+
+let b: number = 20;
+let out2: string = '';
+if (b < 10){
+    out2 = "less";
+}else if (b > 20){
+    out2 = "more";
+} else {
+    out2 = "sweet";
+}
+console.log(out2);
+
+let c: number = 123;
+let credits: number = 10;
+let isBonus: boolean = false;
+
+if(credits >= 50 && isBonus == false){
+    c = c - 2;
+}else if (credits < 50 && isBonus == false){
+    c = c - 1;
+}
+console.log(c);
+
+//fizzbuzz
+for (let i: number = 1; i < 101; i++){
+    if(i % 3 === 0){
+        console.log("fizz");
+    }
+    if (i % 5 === 0){
+        console.log("buzz");
+    }
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("fizzbuzz");
+    }else{
+        console.log(i);
+    }
+}
