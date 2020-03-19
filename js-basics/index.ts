c6f2a7bc56427cb84406c42a5fe8407ad7c42e8b
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2020-03-19 16:13:34 +0100

    first ts try

---
+++ js-basics/index.ts
@@ -0,0 +1,33 @@
+'use strict';
+console.log('Hello world!');
+
+let myName: string = 'Orsi';
+console.log(`hello ${myName}`);
+
+// humpty Dumpty
+console.log('Humpty Dumpty sat on a wall,');
+console.log('Humpty Dumpty had a great fall.');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put Humpty together again.');
+
+// Greet 3 of your classmates
+let greet: string = "Hello";
+console.log(greet + ", Esther!");
+console.log(greet + ", Mary!");
+console.log(greet + ", Joe!");
+
+// introduce yourself
+let age: number = 46;
+let height: number = 1.72;
+
+console.log(myName);
+console.log(age);
+console.log(height);
+
+// coding hours
+let semester: number = 17;
+let daily: number = 6;
+let totalHours: number = semester * daily * 5;
+console.log(totalHours);
+let averageDailyWorkHours: number = 52 / 5;
+console.log((daily / averageDailyWorkHours * 100 ).toFixed(2)  + '%');
