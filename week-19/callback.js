7203192b113d0fc71330f355987cca0891d0e706
zvorsi <orsolya.zdeborskyvadasz@gmail.com>
2020-03-25 20:44:03 +0100

    giphy 16

--- week-19/callback.js
+++ week-19/callback.js
@@ -1,19 +1,11 @@
 'use strict'
 
-
+//set timeout
 function printApple() {
     console.log("apple");
     //setTimeout(printApple, 3000);
 }
 printApple();
-
-// Write a program that prints the 
-// following fruits:
-// apple -> immediately
-// pear -> after 1 seconds
-// melon -> after 3 seconds
-// grapes -> after 5 seconds
-
 
 setTimeout(function() {
     console.log("pear");
@@ -26,3 +18,49 @@ setTimeout(function(){
 setTimeout(function(){
     console.log("grapes");
 }, 9000);
+
+
+//callback
+const mapWith = (array, callback) => {
+    let output = [];
+  
+    // The mapWith() function should iterate over the given array and call the callback function on every element.
+    // It stores the callback return values in the output.
+    // The mapWith() should return with the output array.
+  
+    for (const element of array){
+        output.push(callback(element));
+    }
+    return output;
+  };
+  
+  const addOne = (number) => {
+    return number + 1;
+  };
+  
+  // Exercise 1:
+  
+  console.log(mapWith([1, 2, 3], addOne));
+  //expected result: [2, 3, 4]
+  
+  // Exercise 2:
+  
+  // Create a callback function which removes every second character from a string
+
+  const removeSecondLetter = (word) => {
+      const tempArray = [];
+
+      word.split('').array.forEach(element, index) => {
+          if(index % 2 !== 0){
+              element = '';
+          }
+          tempArray.push(element);    
+      });
+    word = tempArray
+  };
+  
+  const words = ['map', 'reduce', 'filter'];
+
+  
+  console.log(mapWith(words, removeSecondLetter));
+  // expected result: ['mp','rdc', 'fle']
