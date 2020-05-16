1da163cdfc81786e9dfff224928f90fbf1262c24
zvorsi <56042401+zvorsi@users.noreply.github.com>
2020-05-16 17:44:10 +0200

    rddit again

--- week-21/reddit/reddit.js
+++ week-21/reddit/reddit.js
@@ -12,43 +12,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const mysql = require('mysql');
 const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'orsi',
-  password: 'password',
-  database: 'reddit'
+    host: 'localhost',
+    user: 'orsi',
+    password: 'password',
+    database: 'reddit'
 });
 
 
 app.get('/hello', (req, res) => {
     res.send('Hello World');
-  });
+});
 
 
 app.get('/reddit', (req, res) => {
 
-  conn.query('SELECT * FROM reddit.reddit ORDER BY like_counter DESC', (err, rows) =>{ //a desc után majd egy 'limit 3' is kell, ha a lapozás meglesz
-    if (err){
-      console.log('error', err);
-      res.status(500);
-    }else{
-    res.render('index', {
-      rows
+    conn.query('SELECT * FROM reddit.reddit ORDER BY like_counter DESC', (err, rows) => { //a desc után majd egy 'limit 3' is kell, ha a lapozás meglesz
+        if (err) {
+            console.log('error', err);
+            res.status(500);
+        } else {
+            res.render('index', {
+                rows
+            });
+        }
     });
-  }
-  });
 
 });
 
 app.get('/new-post', (req, res) => {
 
     conn.query('SELECT * FROM reddit.reddit ORDER BY id DESC LIMIT 10;', (err, rows) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.render('new-post', {
-            rows
-        });
-      }
+        if (err) {
+            res.send(err);
+        } else {
+            res.render('new-post', {
+                rows
+            });
+        }
     });
 
 });
@@ -60,47 +60,73 @@ app.post('/new-post', (req, res) => {
     let timestamp = new Date(); //vagy lehet, hogy ez kell ide: new date().toLocaleDateString();
 
     conn.query('INSERT INTO reddit.reddit (title, url, post_date) VALUES (?, ?, ?)', [title, url, timestamp], (err, rows) => {
-        if(err){
+        if (err) {
             res.send(err);
-        }else{
+        } else {
             res.redirect('/reddit');
         }
     });
 });
 
 
+app.get('/reddit/:id/upvote', (req, res) => {
+    conn.query('SELECT * FROM reddit.reddit ORDER BY id DESC LIMIT 10;', (err, rows) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.render('index', {
+                rows
+            });
+        }
+    });
+});
 
 
-app.put('/upvote/:id', (req, res) => {
+app.put('/reddit/:id/upvote', (req, res) => {
+    // res.setHeader('Content-Type', 'application/json');
     let post_id = req.params.id;
-    conn.query(`UPDATE reddit.reddit SET like_counter = like_counter + 1 WHERE id = ${req.params.id}`, [id], (err, rows) => {
-      if (err) {
-        res.status(500).send('Database error');
-      } else {
-        conn.query(`SELECT * FROM reddit.reddit WHERE id = ${req.params.id}`, [id], (err, rows) => {
-          res.redirect('/reddit');
-        })
-      }
+    conn.query('UPDATE reddit.reddit SET like_counter = like_counter + 1 WHERE id = ?', [post_id], (err, rows) => {
+        if (err) {
+            res.status(500).send('Database error');
+        } else {
+            conn.query(`SELECT * FROM reddit.reddit WHERE id = ?`, [post_id], (err, rows) => {
+                res.render('index', {
+                    rows
+                });
+            })
+        }
     })
-  });
-  
-  app.put('/downvote/:id', (req, res) => {
+});
+
+app.get('/reddit/:id/downvote', (req, res) => {
+    conn.query('SELECT * FROM reddit.reddit ORDER BY id DESC LIMIT 10;', (err, rows) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.render('index', {
+                rows
+            });
+        }
+    });
+});
+
+
+app.put('/reddit/:id/downvote', (req, res) => {
     let post_id = req.params.id;
-    conn.query(`UPDATE reddit.reddit SET like_counter = like_counter - 1 WHERE id = ${req.params.id}`, [id], (err, rows) => {
-      if (err) {
-        res.status(500).send('Database error');
-      } else {
-        conn.query(`SELECT * FROM posts WHERE id = ${req.params.id}`, [id], (err, rows) => {
-          res.redirect('/reddit');
-        })
-      }
+    conn.query('UPDATE reddit.reddit SET like_counter = like_counter - 1 WHERE id = ?', [post_id], (err, rows) => {
+        if (err) {
+            res.status(500).send('Database error');
+        } else {
+            conn.query(`SELECT * FROM posts WHERE id = ?`, [post_id], (err, rows) => {
+                res.render('index', {
+                    rows
+                });
+            })
+        }
     })
-  });
-
-
-
+});
 
 
 app.listen(3000, () => {
-  console.log('the app is listening');
+    console.log('the app is listening');
 });
